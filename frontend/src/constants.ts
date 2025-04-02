ed7ce9c34579afe04fd1e9dde19f05850ff49076
phzamora <p.hzamora@icloud.com>
2025-04-02 23:21:20 +0200

    update: constants

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,2 +1,6 @@
 export const API_VERSION = import.meta.env.VITE_API_VERSION || 'v1';
-export const API_URL = `/api/${API_VERSION}`;
+export const API_PATH = `api/${API_VERSION}`;
+
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+
+export const API_URL= `${API_BASE_URL}/${API_PATH}` // "http://localhost:8000/api/v1"
