79d37bd2034ea90dd5a27df3fc5c0342e2da1cc6
phzamora <p.hzamora@icloud.com>
2025-04-02 15:50:02 +0200

    chore: added 'swagger_ui_parameters' in FastAPI class

--- backend/src/main.py
+++ backend/src/main.py
@@ -64,6 +64,9 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(
+    swagger_ui_parameters={
+        "syntaxHighlight.theme": "obsidian",
+    },
     docs_url="/docs" if ENV == "dev" else None,
     openapi_url="/openapi.json" if ENV == "dev" else None,
     redoc_url=None,
