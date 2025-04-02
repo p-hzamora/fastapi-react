a3243d315ba91319c8e08eb0ceea7abb5e7867c4
phzamora <p.hzamora@icloud.com>
2025-04-02 23:19:39 +0200

    update: uvicorn

--- backend/src/main.py
+++ backend/src/main.py
@@ -130,4 +130,4 @@ app.include_router(user.router, prefix=f"{API_URI}/user", tags=["user"])
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, port=8000)
